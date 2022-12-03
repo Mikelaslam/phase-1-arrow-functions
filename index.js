6676a40ba12bbe0ce5768c1440929a1a5cde96f7
mikelaslam <kipkorir385@gmail.com>
2022-12-03 13:06:34 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+//wrire your code here
+const divide = function(){
+   return 2000/100
+}
+
+//arrow function called square
+  const square = x => x ** 2;
+
+  //arrow function  called add
+  const add = (parameter1, parameter2) => parameter1 + parameter2;
+
+
+        
